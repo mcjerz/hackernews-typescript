ceb686ea78dfb73c696d36d9920ffea204161d70
alyson albasi <aly@mcjerz.net>
2022-05-23 17:10:38 -0400

    adds Get and Post functionality

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { extendType, idArg, intArg, list, nonNull, objectType, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
@@ -26,5 +26,44 @@ let links: NexusGenObjects["Link"][]= [
         id: 3,
         url: "github.com/mcjerz",
         description: "my github",
+    },
+];
+
+export const LinkQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("feed", {
+            type: "Link",
+            resolve(parent, args, context, info) {
+                return links;
+            }
+        })
     }
-]
+});
+
+export const LinkMutation = extendType({
+    type: "Mutation",
+    definition(t) {
+        t.nonNull.field("post", {
+            type: "Link",
+            args: {
+                description: nonNull(stringArg()),
+                url: nonNull(stringArg()),
+            },
+            resolve(parent, args, context) {
+                const { description, url} = args;
+
+                let idCount = links.length + 1;
+                const link = {
+                    id: idCount,
+                    description: description,
+                    url: url,
+                };
+                links.push(link);
+                return link;
+            },
+        });
+    },
+});
+
+
